89c366f8fb2af608f23df0776683500a7e3fc017
DiegoOno <diego.ono1505@gmail.com>
2018-06-16 19:00:33 -0300

    Desenhando todas as formas

--- canvas.js
+++ canvas.js
@@ -1,61 +1,108 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const click = new Event('click');
-var pos = {
-    x: 0,
-    y: 0
-};
-// var numberClicks = 0;
 
-// function getXY(){
-//     canvas.addEventListener('click', (e) => {
-//         pos = {
-//             x: e.clientX - canvas.offsetLeft,
-//             y: e.clientY - canvas.offsetTop
-//         };
-//         numberClicks++;
-//         console.log('X = ' + pos.x + ' Y = ' + pos.y);
-//         }, false);
-// }
+function get2Points(event, form){
+    var x0, x1, y0, y1;
+    function distance2Points(){
+        var distance =  Math.sqrt(Math.pow(x1-x0,2) + Math.pow(y1-y0, 2));
+        return distance;
+    }
 
+    function removeEvent(){
+        form = null;
+    }
+    
+    canvas.onmousedown = function(event){
+        event = event;
+        x0 = event.clientX - canvas.offsetLeft;
+        y0 = event.clientY - canvas.offsetTop;
+        console.log('X0 = ' + x0 + ' Y0 = ' + y0)
+    }
 
-// function drawLine(){
-//     numberClicks = 0;
-//     totalClicks = 2;
-//     ctx.beginPath();
-//     alert("É necessário fornecer " + (totalClicks-numberClicks) + " pontos no canvas.");
-//     ctx.moveTo(pos.x, pos.y);
-//     ctx.lineTo(pos.x, pos.y);
-//     ctx.stroke();
-// }
+    canvas.onmouseup = function(event){
+        event = event;
+        x1 = event.clientX - canvas.offsetLeft;
+        y1 = event.clientY - canvas.offsetTop;
+        console.log('X1 = ' + x1 + ' Y1 = ' + y1);
+        switch(form){
+            case 'line':
+                ctx.beginPath();
+                ctx.moveTo(x0, y0);
+                ctx.lineTo(x1, y1);
+                ctx.stroke();
+                removeEvent();        
+                break;
+            case 'circle':
+                ctx.beginPath();
+                ctx.arc(x0, y0, distance2Points(), 0, 2*Math.PI);
+                ctx.stroke();
+                removeEvent();
+                break;
+            case 'rect':
+                ctx.beginPath();
+                ctx.moveTo(x0, y0);
+                ctx.lineTo(x1, y0);
+                ctx.lineTo(x1, y1);
+                ctx.lineTo(x0, y1);
+                ctx.lineTo(x0, y0);
+                ctx.stroke();
+                removeEvent();
+                break;
+            default:
+                console.log('Não faz nada');
+        }
+    }
+}
 
-// function drawCircle(){
-//     numberClicks = 0;
-//     totalClicks = 2;
-//     ctx.beginPath();
-//     ctx.arc(pos.x, pos.y, 150, 0, 2*Math.PI);
-//     ctx.stroke();
-// }
+function get3Points(event){
+    var x0, x1, x2, y0, y1, y2, x, y
+    var numberClicks = 0;
+    
+    canvas.onclick = function(event){
+        x = event.clientX - canvas.offsetLeft;
+        y = event.clientY - canvas.offsetTop;
+        console.log('X = ' + x + ' Y = ' + y)
+        numberClicks++;
+        if (numberClicks == 1){
+            x0 = x;
+            y0 = y;
+        }
+    
+        if (numberClicks == 2){
+            x1 = x;
+            y1 = y;
+        }
+    
+        if (numberClicks == 3){
+            x2 = x;
+            y2 = y;
+            ctx.beginPath();
+            ctx.moveTo(x0, y0);
+            ctx.lineTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.lineTo(x0, y0);
+            ctx.stroke();
+        }
+    }
+}
 
-// function drawRectangle(){
-//     numberClicks = 0;
-//     totalClicks = 4;
-//     ctx.beginPath();
-//     ctx.strokeRect(50, 50, 50, 50);
-// }
+function drawLine(){
+    console.log("Você escolheu desenhar uma linha.");
+    get2Points(click, 'line');    
+}
 
-// function drawTriangle(){
-//     numberClicks = 0;
-//     totalClicks = 3;
-//     ctx.beginPath();
-//     ctx.stroke();
-// }
+function drawCircle(){
+    get2Points(click, 'circle');
+}
 
-// function drawPoligon(){
-//     numberClicks = 0;
-//     ctx.beginPath();
-//     ctx.stroke();   
-// }
+function drawRectangle(){
+    get2Points(click, 'rect');
+}
+
+function drawTriangle(){
+    get3Points(click);
+}
 
 function clearCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
