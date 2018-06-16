6f9e108b85ef475822351f23caf826c0236d39d0
DiegoOno <diego.ono1505@gmail.com>
2018-06-16 11:07:54 -0300

    Começando as classes

--- canvas.js
+++ canvas.js
@@ -1,56 +1,61 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const click = new Event('click');
 var pos = {
     x: 0,
     y: 0
 };
+// var numberClicks = 0;
 
-canvas.addEventListener('click', (e) => {
-    pos = {
-        x: e.clientX,
-        y: e.clientY
-    };
-    console.log('X | Y = ' + pos.x + '|' + pos.y);
-}, 
-false);
+// function getXY(){
+//     canvas.addEventListener('click', (e) => {
+//         pos = {
+//             x: e.clientX - canvas.offsetLeft,
+//             y: e.clientY - canvas.offsetTop
+//         };
+//         numberClicks++;
+//         console.log('X = ' + pos.x + ' Y = ' + pos.y);
+//         }, false);
+// }
 
 
-function drawLine(){
-    ctx.beginPath();
-    ctx.moveTo(pos.x, pos.y);
-    ctx.lineTo(pos.x, pos.y);
-    ctx.stroke();
-}
+// function drawLine(){
+//     numberClicks = 0;
+//     totalClicks = 2;
+//     ctx.beginPath();
+//     alert("É necessário fornecer " + (totalClicks-numberClicks) + " pontos no canvas.");
+//     ctx.moveTo(pos.x, pos.y);
+//     ctx.lineTo(pos.x, pos.y);
+//     ctx.stroke();
+// }
 
-function drawCircle(){
-    ctx.beginPath();
-    ctx.arc(pos.x, pos.y, 100, 0, 2*Math.PI);
-    ctx.stroke();
-}
+// function drawCircle(){
+//     numberClicks = 0;
+//     totalClicks = 2;
+//     ctx.beginPath();
+//     ctx.arc(pos.x, pos.y, 150, 0, 2*Math.PI);
+//     ctx.stroke();
+// }
 
-function drawRectangle(){
-    ctx.beginPath();
-    ctx.strokeRect(50, 50, 50, 50);
-}
+// function drawRectangle(){
+//     numberClicks = 0;
+//     totalClicks = 4;
+//     ctx.beginPath();
+//     ctx.strokeRect(50, 50, 50, 50);
+// }
 
-function drawTriangle(){
-    ctx.beginPath();
-    ctx.moveTo(75,50);
-    ctx.lineTo(100,75);
-    ctx.lineTo(100,25);
-    ctx.lineTo(75,50);
-    ctx.stroke();
-}
+// function drawTriangle(){
+//     numberClicks = 0;
+//     totalClicks = 3;
+//     ctx.beginPath();
+//     ctx.stroke();
+// }
 
-function drawPoligon(){
-    ctx.beginPath();
-    ctx.moveTo(150, 150);
-    ctx.lineTo(200,200);
-    ctx.lineTo(200, 250);
-    ctx.lineTo(300, 250);
-    ctx.lineTo(150, 150);
-    ctx.stroke();   
-}
+// function drawPoligon(){
+//     numberClicks = 0;
+//     ctx.beginPath();
+//     ctx.stroke();   
+// }
 
 function clearCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
---
+++ line.js
@@ -0,0 +1,24 @@
+function line()
+{
+    this.X0 = setX0;
+    this.Y0 = setY0;
+    this.X1 = setX1;
+    this.Y1 = setY1;
+
+    function setX0(_X0){
+        X0 = _X0;
+    }
+
+    function setY0(_Y0){
+        Y0 = _Y0;
+    }
+
+    function setX1(_X1){
+        X1 = _X1;
+    }
+
+    function setY1(_Y1){
+        Y1 = _Y1;
+    }
+
+}
