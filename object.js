79b4ccf2a4bf05cdd1aeace20da9818f5adf700d
DiegoOno <diego.ono1505@gmail.com>
2018-06-24 22:24:56 -0300

    Arrumando espaçamento

--- object.js
+++ object.js
@@ -27,6 +27,7 @@ class Line {
 }
 
 class Circle {
+    
     constructor(x0, y0, radius) {
         this.objectName = 'circle';
         this.x0 = x0;
@@ -37,20 +38,27 @@ class Circle {
             this.objectMatrix[i] = new Array(1);
         }
     }
+
     makeMatrix() {
         this.objectMatrix[0][0] = this.x0;
         this.objectMatrix[1][0] = this.y0;
         this.objectMatrix[2][0] = 1;
     }
+
     draw(ctx) {
         ctx.beginPath();
         ctx.arc(this.x0, this.y0, this.radius, 0, 2*Math.PI);
         ctx.stroke();
     }
+
+    remove() {
+
+    }
   
 }
 
 class Rect {
+    
     constructor(x0, y0, x1, y1) {
         this.objectName = 'rect';
         this.x0 = x0;
@@ -62,6 +70,7 @@ class Rect {
             this.objectMatrix[i] = new Array(4);
         }
     }
+    
     makeMatrix() {
         this.objectMatrix[0][0] = this.x0;
         this.objectMatrix[1][0] = this.y0;
@@ -76,6 +85,7 @@ class Rect {
         this.objectMatrix[1][3] = this.y0;
         this.objectMatrix[2][3] = 1;
     }
+    
     draw(ctx){
         ctx.beginPath();
         ctx.moveTo(this.x0, this.y0);
@@ -88,6 +98,7 @@ class Rect {
 }
 
 class Triangle {
+    
     constructor(x0, y0, x1, y1, x2, y2) {
         this.objectName = 'triangle';
         this.x0 = x0;
@@ -101,6 +112,7 @@ class Triangle {
             this.objectMatrix[i] = new Array(3);
         }
     }
+    
     makeMatrix() {
         this.objectMatrix[0][0] = this.x0;
         this.objectMatrix[1][0] = this.y0;
@@ -112,6 +124,7 @@ class Triangle {
         this.objectMatrix[1][2] = this.y2;
         this.objectMatrix[2][2] = 1;
     }
+    
     draw(ctx){
         ctx.beginPath();
         ctx.moveTo(this.x0, this.y0);
